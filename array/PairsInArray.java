3501036a206a3ee2e740c0a09c20f951001e8e42
DheerajChopra <chopradheeraj11@gmail.com>
2024-02-06 21:13:08 +0530

    Pairs in Array commit

---
+++ array/PairsInArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PairsInArray{
+    public static void Pairs(int numbers[]){
+        for(int i=0; i<numbers.length; i++){
+            int curr=numbers[i]; //2,4,6,8,10
+            for(int j=i+1; j<numbers.length; j++){
+                System.out.print("("+curr+","+numbers[j]+")");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String args[]){
+        int numbers[]={2,4,6,8,10};
+        Pairs(numbers);
+
+    }    
+}
