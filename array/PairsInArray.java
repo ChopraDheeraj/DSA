3fdebad8b4bc21e5f792333784a1a9d78ebd38bf
DheerajChopra <chopradheeraj11@gmail.com>
2024-02-06 21:18:03 +0530

    commit

--- array/PairsInArray.java
+++ array/PairsInArray.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class PairsInArray{
     public static void Pairs(int numbers[]){
+        int tp=0;
         for(int i=0; i<numbers.length; i++){
             int curr=numbers[i]; //2,4,6,8,10
             for(int j=i+1; j<numbers.length; j++){
                 System.out.print("("+curr+","+numbers[j]+")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs: "+ tp);
     }
 
 
