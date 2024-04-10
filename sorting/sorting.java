e3e58bf8d9f6ad5b0ffa6fa36c02d2acff975044
DheerajChopra <chopradheeraj11@gmail.com>
2024-04-10 09:45:58 +0530

    sort

---
+++ sorting/sorting.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+//bubble sort in descending order
+
+public class sorting{
+    /* 
+    public static void Bubble(int arr[]){
+        for(int turns=0; turns<arr.length; turns++){
+            for(int j=0; j<arr.length-1-turns; j++){
+                if(arr[j]<arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    */
+
+    
+        public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]={3,6,2,1,8,7,4,5,3,1};
+        Bubble(arr);
+        printArr(arr);
+    }
+}
