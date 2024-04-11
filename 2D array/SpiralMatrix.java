2cb183a5af9632bf4f9281f7b511727315790bf3
DheerajChopra <chopradheeraj11@gmail.com>
2024-04-11 18:18:03 +0530

    spiral matrix

---
+++ 2D array/SpiralMatrix.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class SpiralMatrix{
+
+    public static void Spiral(int matrix[][]){
+        int startCol=0;
+        int startRow=0;
+        int endCol=matrix[0].length-1;
+        int endRow=matrix.length-1;
+
+        while(endRow >= startRow && endCol >= startCol){
+            //top
+            for(int j=startCol; j<=endCol; j++){
+                System.out.print(matrix[startRow][j]+" ");
+            }
+            //right
+            for(int i=startRow+1; i<=endRow; i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //bottom
+            for(int j=endCol-1; j>=startCol; j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            //left
+            for(int i=endRow-1; i>startRow; i--){
+                if(startCol==endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        Spiral(matrix);
+    }
+}
