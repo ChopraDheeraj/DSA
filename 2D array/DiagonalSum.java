691a3ae55f5ac165bdd47a7238ffbac89b723061
DheerajChopra <chopradheeraj11@gmail.com>
2024-04-11 23:54:08 +0530

    diagonal

---
+++ 2D array/DiagonalSum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class DiagonalSum{
+    public static int diagonal(int matrix[][]){
+        int sum=0;
+        for(int i=0; i<matrix.length; i++){
+            //pd
+            sum += matrix[i][i];
+            //sd
+            if(i != matrix[i][matrix.length-i-1]){
+                sum+=matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+    }
+    
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16,}};
+        System.out.print(diagonal(matrix));
+    }
+}
