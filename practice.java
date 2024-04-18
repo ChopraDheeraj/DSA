14834edb842b83a4b1e0c0c723a993fef03e4601
DheerajChopra <chopradheeraj11@gmail.com>
2024-04-18 11:09:28 +0530

    save

--- practice.java
+++ practice.java
@@ -2,45 +2,14 @@ import java.util.*;
 
 public class practice{
 
-    public static void Spiral(int matrix[][]){
-        int startCol=0;
-        int startRow=0;
-        int endCol=matrix[0].length-1;
-        int endRow=matrix.length-1;
-
-        while(endRow >= startRow && endCol >= startCol){
-            //top
-            for(int j=startCol; j<=endCol; j++){
-                System.out.print(matrix[startRow][j]+" ");
-            }
-            //right
-            for(int i=startRow+1; i<=endRow; i++){
-                System.out.print(matrix[i][endCol]+" ");
-            }
-            //bottom
-            for(int j=endCol-1; j>=startCol; j--){
-                if(startRow==endRow){
-                    break;
-                }
-                System.out.print(matrix[endRow][j]+" ");
-            }
-            //left
-            for(int i=endRow-1; i>startRow; i--){
-                if(startCol==endCol){
-                    break;
-                }
-                System.out.print(matrix[i][startCol]+" ");
-            }
-            startRow++;
-            startCol++;
-            endRow--;
-            endCol--;
-        }
-        System.out.println();
-    }
-
     public static void main(String args[]){
-        int matrix[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-        Spiral(matrix);
+        String fruit[]={"apple","mango","banana"};
+        String largest=fruit[0];
+        for(int i=0; i<fruit.length; i++){
+            if(largest.compareTo(fruit[i])<0){
+                largest=fruit[i];
+            }
+        }
+        System.out.println(largest);        
     }
 }
