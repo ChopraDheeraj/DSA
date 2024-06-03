0af31006c858d14568527fe8aab48d3fb1755961
DheerajChopra <chopradheeraj11@gmail.com>
2024-06-03 15:47:23 +0530

    t

--- practice.java
+++ practice.java
@@ -2,29 +2,17 @@ import java.util.*;
 
 public class practice{
 
-    public static int hello(int num[], int key){
-        int start=1;
-        int end=num.length;
-        while(end>=start){
-            //mid
-            int mid=(start+end)/2;
-            if(num[mid]==key){
-                return mid;
-            }
-            
-            if(num[mid]>key){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
+    public static int Factorial(int n){
+        if(n==0){
+            return 1;
         }
-        return -1;
+        int fnm1=Factorial(n-1);
+        int fac=n*Factorial(n-1);
+        return fac;
     }
 
     public static void main(String args[]){
-        int num[]={2,4,6,8,10};
-        int key=6;
-        System.out.println(hello(num, key));
+        int n = 5;
+        System.out.print(Factorial(n));
     }
 }
