2c8a352400b483be9587cc02b16b62079917dc25
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-02 11:51:38 +0530

    c

---
+++ Stack/PushAtBottom.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PushAtBottom{
+
+    public static void pushBottom(Stack<Integer> s, int data){
+        //base case
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushBottom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushBottom(s,4);
+        while(!s.isEmpty()){
+            System.out.println(s.pop()); 
+        }
+    }
+}
