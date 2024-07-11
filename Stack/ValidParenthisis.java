ede6fc3133faf9fd893a3634b7fffbe30a01812a
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-11 11:12:10 +0530

    c

---
+++ Stack/ValidParenthisis.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class ValidParenthisis {
+
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            //opening
+            if(ch == '(' || ch =='[' || ch== '{'){
+                s.push(ch);
+            }
+            //closing 
+            else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if( (s.peek() == '(' && ch == ')') 
+                  || (s.peek() == '{' && ch == '}')
+                  || (s.peek() == '[' && ch == ']') ){
+                    s.pop();
+                  }
+                  else{
+                    return false;
+                  }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String args[]){
+        String str = "((){})[]";
+        System.out.println(isValid(str));
+    }
+}
