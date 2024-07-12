2109ebf9b13d89b4ad346302350ab70f9c868d4c
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-12 18:32:58 +0530

    c

---
+++ Queue/QueueArray.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class QueueArray {
+
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        //hmne front nhi banaya kyuki front is fix
+
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+        
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        //add
+        public static void add(int data){
+            if(rear == size-1){ //queue is full //size=n=last index
+                System.out.println("Queue is full");
+                return;
+            }
+            rear = rear+1;
+            arr[rear] = data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1; //-1 represents there is no no. stored
+            }
+            
+            int front = arr[0];
+            for(int i=0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String args[]){
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
