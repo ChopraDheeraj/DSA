c5b89fb248bc30c71a7deceab0001fd82a87e383
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-24 23:17:02 +0530

    c

--- BinaryTrees/InOrderTraversal.java
+++ BinaryTrees/InOrderTraversal.java
@@ -2,7 +2,46 @@ import java.util.*;
 
 public class InOrderTraversal {
 
+    static class Node{
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data){
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+        public static void inOrder(Node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+    }
+
     public static void main(String args[]){
-        
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+
+        tree.inOrder(root);
+
     }
 }
