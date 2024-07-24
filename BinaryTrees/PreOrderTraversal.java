8ad84523a1077effc181b348874e713545e4fbd8
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-24 20:30:05 +0530

    c

---
+++ BinaryTrees/PreOrderTraversal.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class PreOrderTraversal {
+
+    static class Node{
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data){
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    public static void main(String args[]){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+
+        tree.preOrder(root);
+        
+    }
+}
