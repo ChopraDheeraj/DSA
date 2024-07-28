7efbcc10a70753a874e000ba19f6b1206cc6cb94
DheerajChopra <chopradheeraj11@gmail.com>
2024-07-28 22:37:41 +0530

    c

--- BinaryTrees2/TopView.java
+++ BinaryTrees2/TopView.java
@@ -1,74 +1,77 @@
 import java.util.*;
 import java.util.LinkedList;
 
-public class TopView {
+public class TopView{
 
-    static class Node {
+    static class Node{
         int data;
         Node left;
         Node right;
 
-        Node(int data) {
+        Node(int data){
             this.data = data;
-            this.right = null;
             this.left = null;
+            this.right = null;
         }
     }
 
-    static class Info {
+    static class Info{
         Node node;
         int hd;
 
-        public Info(Node node, int hd) {
+        public Info(Node node, int hd){
             this.node = node;
             this.hd = hd;
         }
     }
 
-    public static void toppView(Node root) {
-        if (root == null) {
-            return;
-        }
-
+    public static void topView(Node root){
         Queue<Info> q = new LinkedList<>();
         HashMap<Integer, Node> map = new HashMap<>();
-        int min = 0, max = 0; // Initialize min and max
+
+        int min=0, max = 0;
         q.add(new Info(root, 0));
+        q.add(null);
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Info curr = q.remove();
-
-            if (!map.containsKey(curr.hd)) { // 1st time my hd is occurring
-                map.put(curr.hd, curr.node);
+            if(curr==null){
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
             }
-
-            if (curr.node.left != null) {
-                q.add(new Info(curr.node.left, curr.hd - 1));
-                min = Math.min(min, curr.hd - 1);
-            }
-
-            if (curr.node.right != null) {
-                q.add(new Info(curr.node.right, curr.hd + 1));
-                max = Math.max(max, curr.hd + 1); // Update max
+            else{
+                if(!map.containsKey(curr.hd)){
+                    map.put(curr.hd, curr.node);
+                }
+                if(curr.node.left != null){
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min = Math.min(min, curr.hd-1);
+                }
+                if(curr.node.right != null){
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max = Math.max(max, curr.hd+1);
+                }
             }
         }
-
-        for (int i = min; i <= max; i++) {
-            if (map.get(i) != null) {
-                System.out.print(map.get(i).data + " ");
-            }
+            for(int i = min; i<=max; i++){
+                System.out.print(map.get(i).data+" ");
         }
-        System.out.println();
+         System.out.println();
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
+        root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        toppView(root);
+        topView(root);
     }
 }
