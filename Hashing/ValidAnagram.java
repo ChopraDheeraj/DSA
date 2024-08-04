98e64b9ad9db1e1ebd8e9ca972f02e1c168798f9
DheerajChopra <chopradheeraj11@gmail.com>
2024-08-04 19:36:14 +0530

    c

---
+++ Hashing/ValidAnagram.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+
+        for(int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.get(ch)!= null){   //agar map mei ch vali key exist krti h
+                if(map.get(ch) == 1){  //aur agar freq=1
+                    map.remove(ch);    //fir element remove krdo
+                }
+                else{
+                    map.put(ch, map.get(ch) - 1);  //aur agar freq 1 se badi h, usko -1 krdena h
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return map.isEmpty();
+    }
+
+    
+    
+    public static void main(String args[]){
+        String s = "race";
+        String t = "care";
+        System.out.print(isAnagram(s, t));
+    }
+}
--- Strings/BasicString.java
+++ Strings/BasicString.java
@@ -3,7 +3,7 @@ import java.util.*;
 public  class BasicString{
 
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String name=sc.next();
         System.out.print("Your name is: "+name+", Length is: "+name.length()+" & Character at 1 is: "+name.charAt(1));
         
