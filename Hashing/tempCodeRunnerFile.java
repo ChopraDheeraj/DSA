3832a5d7cc1f911b6ca05cb7852159922f64e48d
DheerajChopra <chopradheeraj11@gmail.com>
2024-08-05 10:51:31 +0530

    c

---
+++ Hashing/tempCodeRunnerFile.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Union {
+
+    public static void main(String args[]){
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,2,4};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        //union
+        for(int i=0; i<arr1.length; i++){
+            set.add(arr1[i]);
+        }
+        for(int i=0; i<arr2.length; i++){
+            set.add(arr2[i]);
+        }
+        System.out.println("Union is: "+set.size());
+        for (Integer se : set) {
+            System.out.print(se+" ");
+        }
+        System.out.println();
+
+        //intersection
+        set.clear();
+        for(int i=0; i<arr1.length; i++){
+            set.add(arr1[i]);
+        }
+        
+        int count=0;
+        for(int i=0; i<arr2.length; i++){
+            if(set.contains(arr2[i])){
+                set.remove(arr2[i]);
+                count++;  //so that baar baar count na ho
+                for (Integer it : set) {
+                    System.out.print(it+" ");
+                }
+            }
+            
+        }
+        System.out.println("Intersection is "+ count);
+   
+    }
+}
