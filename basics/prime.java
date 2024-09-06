31d7fc3bdf8ba3905631e8b41b25721fb080c2db
DheerajChopra <chopradheeraj11@gmail.com>
2024-09-06 12:36:48 +0530

    c

--- basics/prime.java
+++ basics/prime.java
@@ -1,10 +1,33 @@
-package basics;
+import java.util.*;
+
+public class prime{
+
+    public static boolean isPrimerange(int n){
+        //base case
+        if(n==2){
+            return true;
+        }
+
+        for(int i = 2; i<n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        System.out.print(isPrimerange(10));
+    }
+}
+
+//primes in range
+/*package basics;
 import java.util.*;
 
 public class prime{
   
     public static void primesinRange(int n){
-        int isPrimerange=true;
         for(int i=2; i<=n; i++){
             if(isPrimerange(i)){
                 System.out.println(i+" ");
@@ -17,3 +40,4 @@ public class prime{
 
     }
 }
+    */
