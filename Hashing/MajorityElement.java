4bc3e0aeafa66c09f416b16e796459a4babb8732
DheerajChopra <chopradheeraj11@gmail.com>
2024-09-21 22:17:06 +0530

    c

--- Hashing/MajorityElement.java
+++ Hashing/MajorityElement.java
@@ -22,3 +22,21 @@ public class MajorityElement {
         }
     }
 }
+
+
+/*
+ * class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        for(Integer maps : map.keySet()){
+            if(map.get(maps) > (nums.length)/2){
+                return maps;
+            }
+        }
+        return -1;
+    }
+}
+ */
