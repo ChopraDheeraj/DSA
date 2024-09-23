9df981c25c49d4afdecd9d0ebd8ffb81d780eb8c
DheerajChopra <chopradheeraj11@gmail.com>
2024-09-23 22:29:26 +0530

    c

--- Random/Binarysearch.java
+++ Random/Binarysearch.java
@@ -1,31 +1,29 @@
 import java.util.*;
 
-public class Binarysearch {
-
-    public static int BSs(int arr[], int key){
-        int start = 0 ;
-        int end = arr.length - 1;
+public class Binarysearch{
+    
+    public static int bs(int arr[], int key){
+        int start = 0, end = arr.length-1;
         
-        while(end>=start){
+        while(start <= end){
             int mid = (start+end)/2;
-            if(arr[mid] == key ){
+            if(arr[mid] == key){
                 return mid;
             }
-            if(arr[mid] < key){
-                start = mid + 1;
+            if(arr[mid] > key){
+                end = mid - 1;
             }
             else{
-                end = mid - 1;
+                start = mid + 1;
             }
         }
         return -1;
-
     }
-    
-    public static void main(String args[]){
-        int arr[] = {2,4,6,8,10,12,14};
-        int key = 5;
-        System.out.print(BSs(arr, key));
 
+
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6,7};
+        int key = 6;
+        System.out.print(bs(arr,key));
     }
 }
