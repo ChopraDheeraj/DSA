46f43c95ef64c41185fe78fcae8dd72fe0e6daa9
DheerajChopra <chopradheeraj11@gmail.com>
2024-09-23 22:54:49 +0530

    c

--- array/PairsInArray.java
+++ array/PairsInArray.java
@@ -6,7 +6,7 @@ public class PairsInArray{
         for(int i=0; i<numbers.length; i++){
             int curr=numbers[i]; //2,4,6,8,10
             for(int j=i+1; j<numbers.length; j++){
-                System.out.print("("+curr+","+numbers[j]+")");
+                System.out.print("(" + curr + "," + numbers[j] + ")");
                 tp++;
             }
             System.out.println();
