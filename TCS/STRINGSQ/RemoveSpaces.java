b8adcb27e6f295c6a9c4ac352e4d845124b086dd
DheerajChopra <chopradheeraj11@gmail.com>
2024-10-02 19:50:49 +0530

    c

---
+++ TCS/STRINGSQ/RemoveSpaces.java
@@ -0,0 +1,28 @@
+// remove spaces from a string
+
+import java.util.*;
+
+public class RemoveSpaces{
+    
+    public static String removeSpace(String s){
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0; i<sb.length(); i++){
+            char ch = sb.charAt(i);
+            if(ch == ' '){
+                sb.deleteCharAt(i);
+                i--;
+            }
+        }
+        return sb.toString();
+    }
+    
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String d = removeSpace(s);
+        System.out.print(d);
+        
+        
+    }
+}
