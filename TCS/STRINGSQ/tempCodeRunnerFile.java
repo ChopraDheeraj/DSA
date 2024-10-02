265e7c9c39519c8f848f6f819d658da5636fc537
DheerajChopra <chopradheeraj11@gmail.com>
2024-10-02 19:35:14 +0530

    c

---
+++ TCS/STRINGSQ/tempCodeRunnerFile.java
@@ -0,0 +1,30 @@
+//Check if a given string is palindrome or not
+import java.util.*;
+
+public class Palindrome{
+    public static boolean calc(String s){
+        StringBuilder sb = new StringBuilder(s);
+        int j = sb.length() - 1;
+        for(int i=0; i<s.length()/2; i++){
+            //swap
+            if(sb.charAt(i) != sb.charAt(sb.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        calc(s);
+        
+            if(calc(s) == true){
+                System.out.print("PALINDROME");
+            }
+            else{
+                System.out.println("NOT PALINDROME");
+            }
+        
+    }
+}
